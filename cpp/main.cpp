4d8a71ed7ff5fc5d554807fe62015cc196fa9781
Uy Ha <hchanuy@gmail.com>
2021-05-26 21:46:51 +0200

    Start pushing to a github repo

---
+++ cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int test(){
+  return 1;
+}
+
+int main()
+{
+  test();
+  std::cout << "Hello, World" << std::endl;
+}
---
+++ python/python/__init__.py
@@ -0,0 +1,3 @@
+import scipy
+
+__version__ = '0.1.0'
