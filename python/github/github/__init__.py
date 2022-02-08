fb55cb943e87e4d58c6759788971e2ad987217de
Uy Ha <hchanuy@gmail.com>
2022-02-08 10:27:21 +0100

    Remove github

--- python/github/github/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '0.1.0'
-
-from github import Github
-
-g = Github("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")
-
-for asset in g.get_repo('kewazo/neoflash').get_latest_release().get_assets():
-    print(asset.url)
