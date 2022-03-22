6f2bb9b6d0f9d3a18d1d31323db58ebaaaae1acb
Uy Ha <hchanuy@gmail.com>
2022-03-22 22:51:38 +0100

    cpp: replace magic number with variable

--- cpp/src/benchmark.cpp
+++ cpp/src/benchmark.cpp
@@ -6,19 +6,19 @@
 
 int main() {
   auto const shm_name = "/sandbox.flock.shm";
-  volatile int a      = 1;
 
   auto shm = ::shm_open(shm_name, O_RDONLY | O_CREAT, 0666);
   fmt::print("shm: {}\n", shm);
 
+  constexpr auto loop = 1'000'000;
+
   auto const start = std::chrono::steady_clock::now();
 
-  for (auto i = 0; i < 1000; ++i) {
+  for (auto i = 0; i < loop; ++i) {
     ::flock(shm, LOCK_SH);
-    a = a + 1;
     ::flock(shm, LOCK_UN);
   }
 
   auto const end = std::chrono::steady_clock::now();
-  fmt::print("{}\n", end - start);
+  fmt::print("{}\n", (end - start) / loop);
 }
--- cpp/src/pthread_rwlock.cpp
+++ cpp/src/pthread_rwlock.cpp
@@ -9,13 +9,15 @@ int main() {
   ::pthread_rwlock_t lock;
   fmt::print("{}\n", ::pthread_rwlock_init(&lock, nullptr));
 
+  constexpr auto loop = 1'000'000;
+
   auto const start = std::chrono::steady_clock::now();
-  for (auto i = 0; i < 1'000'000; ++i) {
+  for (auto i = 0; i < loop; ++i) {
     ::pthread_rwlock_wrlock(&lock);
     ::pthread_rwlock_unlock(&lock);
   }
   auto const end = std::chrono::steady_clock::now();
 
   fmt::print("{}\n", ::pthread_rwlock_destroy(&lock));
-  fmt::print("{}\n", (end - start) / 1'000'000);
+  fmt::print("{}\n", (end - start) / loop);
 }
