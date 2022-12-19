69b1cd624e08a36e63ed091e5305eccd28c3ef0a
Uy Ha <hchanuy@gmail.com>
2022-12-19 22:39:35 +0100

    add ccache and precompile more headers

---
+++ zeromq/cmake/Ccache.cmake
@@ -0,0 +1,8 @@
+find_program(CCACHE_EXECUTABLE ccache)
+if(CCACHE_EXECUTABLE)
+  foreach(lang IN ITEMS C CXX OBJC OBJCXX CUDA)
+    set(CMAKE_${lang}_COMPILER_LAUNCHER
+      "${CMAKE_COMMAND}" -E env CCACHE_SLOPPINESS=pch_defines,time_macros "${CCACHE_EXECUTABLE}"
+      )
+  endforeach()
+endif()
