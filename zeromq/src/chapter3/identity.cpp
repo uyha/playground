63b4d7e618beeeeb235ca1abb0b9bdadd523fe49
Uy Ha <hchanuy@gmail.com>
2022-12-25 17:05:40 +0100

    factor out the dump function

--- zeromq/src/chapter3/identity.cpp
+++ zeromq/src/chapter3/identity.cpp
@@ -1,6 +1,18 @@
 #include <fmt/printf.h>
 #include <zmq.hpp>
 
+auto dump(zmq::socket_t &socket) -> void {
+  auto message = zmq::message_t{};
+  fmt::print("----------------------------------------\n");
+  while (true) {
+    assert(socket.recv(message));
+    fmt::print("[{:03}] {}\n", message.size(), message.to_string_view());
+    if (not message.more()) {
+      break;
+    }
+  }
+}
+
 int main() {
   using std::string_literals::operator""s;
 
@@ -12,26 +24,11 @@ int main() {
   auto anonymous = zmq::socket_t{context, zmq::socket_type::req};
   anonymous.connect("inproc://example");
   anonymous.send(zmq::message_t{"ROUTER uses a generated 5 byte identity"s}, zmq::send_flags::none);
-  while (true) {
-    auto message = zmq::message_t{};
-    assert(sink.recv(message));
-    fmt::print("[{:03}] {}\n", message.size(), message.to_string_view());
-    if (not message.more()) {
-      fmt::print("\n");
-      break;
-    }
-  }
+  dump(sink);
 
   auto identified = zmq::socket_t{context, zmq::socket_type::req};
   identified.set(zmq::sockopt::routing_id, "peer2");
   identified.connect("inproc://example");
   identified.send(zmq::message_t{"ROUTER uses REQ's socket identity"s}, zmq::send_flags::none);
-  while (true) {
-    auto message = zmq::message_t{};
-    assert(sink.recv(message));
-    fmt::print("[{:03}] {}\n", message.size(), message.to_string_view());
-    if (not message.more()) {
-      break;
-    }
-  }
+  dump(sink);
 }
