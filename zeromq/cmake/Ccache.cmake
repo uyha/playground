df478b567cd26465730e8e5ee74e654f58cf3e59
Uy Ha <hchanuy@gmail.com>
2023-03-15 23:37:29 +0100

    update cmake modules for zeromq, adapt build script to Conan 2

--- zeromq/cmake/Ccache.cmake
+++ zeromq/cmake/Ccache.cmake
@@ -2,7 +2,6 @@ find_program(CCACHE_EXECUTABLE ccache)
 if(CCACHE_EXECUTABLE)
   foreach(lang IN ITEMS C CXX OBJC OBJCXX CUDA)
     set(CMAKE_${lang}_COMPILER_LAUNCHER
-      "${CMAKE_COMMAND}" -E env CCACHE_SLOPPINESS=pch_defines,time_macros "${CCACHE_EXECUTABLE}"
-      )
+      "${CMAKE_COMMAND}" -E env CCACHE_SLOPPINESS=pch_defines,time_macros "${CCACHE_EXECUTABLE}" PARENT_SCOPE)
   endforeach()
 endif()
