0d30f9f9d7eab02329686ed442cfa35ec1cc1f4a
Uy Ha <hchanuy@gmail.com>
2023-04-23 11:14:08 +0300

    cpp: add script for generating TRANSFORM macro

---
+++ cpp/src/macro_map_gen.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from jinja2 import Environment
+
+limit = 255
+prefix = "NEOBOT_"
+
+result = (
+    Environment()
+    .from_string(
+        """
+// clang-format off
+#define {{prefix}}EVAL(...) __VA_ARGS__
+
+#define {{prefix}}VARCOUNT_IMPL(_,{% for i in range(limit, 1, -1) -%}
+_{{ i }},
+{%- endfor %}result, ...) result
+#define {{prefix}}VARCOUNT(...) {{prefix}}EVAL({{prefix}}VARCOUNT_IMPL(__VA_ARGS__,{{range(limit, 0, -1)|join(',')}},))
+
+#define {{prefix}}GLUE_IMPL(x, y) x##y
+#define {{prefix}}GLUE(x, y) {{prefix}}GLUE_IMPL(x, y)
+
+#define {{prefix}}HEAD_IMPL(x, ...) x
+#define {{prefix}}HEAD(...) {{prefix}}EVAL({{prefix}}HEAD_IMPL(__VA_ARGS__))
+
+#define {{prefix}}TAIL_IMPL(x, ...) (__VA_ARGS__)
+#define {{prefix}}TAIL(...) {{prefix}}EVAL({{prefix}}TAIL_IMPL(__VA_ARGS__))
+
+#define {{prefix}}TRANSFORM(name, args) ({{prefix}}GLUE({{prefix}}TRANSFORM_, {{prefix}}VARCOUNT args)(name, args))
+#define {{prefix}}TRANSFORM_1(name, args) name args
+
+{% for i in range(2, limit +1) -%}
+#define {{prefix}}TRANSFORM_{{ i }}(name, args) name_({{prefix}}HEAD args), {{prefix}}TRANSFORM_{{ i-1 }}(name, {{prefix}}TAIL args)
+{% endfor -%}
+// clang-format on
+"""
+    )
+    .render(limit=limit, prefix=prefix)
+)
+
+print(result)
