f5cbf884d78735f88d3d556d973d6f74f2da35a7
Uy Ha <hchanuy@gmail.com>
2023-08-29 19:53:37 +0000

    update conan

--- cpp/src/conanfile.py
+++ cpp/src/conanfile.py
@@ -1,10 +1,12 @@
 from conan import ConanFile
+from conans.model.requires import Requirements
 
 
 class SandboxRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
+        assert self.requires is not None
         self.requires("fmt/10.0.0")
         self.requires("msgpack-cxx/6.0.0", options={"use_boost": False})
         self.requires("lely-core/2.3.2")
