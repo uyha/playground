9809f68f12307a95cc98328db89eca8085793a22
Uy Ha <hchanuy@gmail.com>
2023-09-02 23:12:39 +0000

    show how to cancel a timer

--- cpp/src/timerfd.cpp
+++ cpp/src/timerfd.cpp
@@ -7,7 +7,7 @@
 #include <sys/timerfd.h>
 #include <unistd.h>
 
-int main() {
+auto basic() -> void {
   auto timer = ::timerfd_create(CLOCK_MONOTONIC, 0);
   auto spec  = ::itimerspec{
        .it_interval = {.tv_sec = 1, .tv_nsec = 0},
@@ -29,4 +29,47 @@ int main() {
     fmt::print("{}:{} {}\n", __FILE__, __LINE__, ::read(timer, &buffer, sizeof(buffer)));
     fmt::print("{}:{} {}\n", __FILE__, __LINE__, std::chrono::system_clock::now());
   }
+}
+
+auto canceling() -> void {
+  auto current = ::timespec{};
+  ::clock_gettime(CLOCK_MONOTONIC, &current);
+
+  auto timer = ::timerfd_create(CLOCK_MONOTONIC, 0);
+  auto spec  = ::itimerspec{
+       .it_interval = {.tv_sec = 0, .tv_nsec = 0},
+       .it_value    = {.tv_sec = 3, .tv_nsec = 0},
+  };
+
+  [[maybe_unused]] auto const cancel = ::itimerspec{
+      .it_interval = {.tv_sec = 0, .tv_nsec = 0},
+      .it_value    = {.tv_sec = 0, .tv_nsec = 0},
+  };
+
+  [[maybe_unused]] auto buffer = std::uint64_t{};
+
+  ::timerfd_settime(timer, 0, &spec, nullptr);
+
+  auto fd = ::pollfd{
+      .fd      = timer,
+      .events  = POLLIN,
+      .revents = {},
+  };
+
+  auto ready_to_read = 0;
+  while (true) {
+    fmt::print("{}:{} {}\n", __FILE__, __LINE__, ready_to_read = ::poll(&fd, 1, 1000));
+
+    // Comment out to stop cancelling
+    ::timerfd_settime(timer, 0, &cancel, nullptr);
+
+    if (ready_to_read != 0) {
+      fmt::print("{}:{} {}\n", __FILE__, __LINE__, ::read(timer, &buffer, sizeof(buffer)));
+    }
+  }
+}
+
+int main() {
+  // basic();
+  canceling();
 }
