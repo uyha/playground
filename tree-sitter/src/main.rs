210c421e72c091e5016307b2e25c194c45b4cfe3
Uy Ha <hchanuy@gmail.com>
2023-09-11 17:33:30 +0000

    implement iterating for EDS

--- tree-sitter/src/main.rs
+++ tree-sitter/src/main.rs
@@ -1,89 +1,110 @@
-use tree_sitter::{Node, Parser, Point, Tree};
+use tree_sitter::{Node, Parser, Tree, TreeCursor};
 use tree_sitter_eds::language;
 
-use std::{env::current_dir, fs};
+use std::fs;
 
 #[derive(Debug)]
-struct Statement {
-    key: String,
-    value: String,
+pub struct EDS {
+    content: String,
+    tree: Tree,
+}
+
+impl EDS {
+    pub fn parse(content: &str) -> Option<EDS> {
+        let mut parser = Parser::new();
+        parser.set_language(language()).ok()?;
+
+        Some(EDS {
+            content: content.to_owned(),
+            tree: parser.parse(content, None)?,
+        })
+    }
 }
 
 #[derive(Debug)]
-struct Section {
-    name: String,
-    statements: Vec<Statement>,
+pub struct EDSNode<'a> {
+    pub content: &'a str,
+    pub node: Node<'a>,
+}
+pub struct EDSIterator<'a> {
+    content: &'a str,
+    cursor: TreeCursor<'a>,
 }
 
-fn eds_parser() -> Parser {
-    let mut parser = Parser::new();
-    parser.set_language(language()).expect("");
-    parser
-}
+impl<'a> IntoIterator for &'a EDS {
+    type Item = EDSNode<'a>;
+    type IntoIter = EDSIterator<'a>;
 
-fn parse_eds(root_node: Node, content: &[u8]) -> Vec<Section> {
-    let mut result = vec![];
-
-    let mut cursor = root_node.walk();
-    let mut current_section: Option<Section> = Option::None;
-
-    loop {
-        let kind = cursor.node().kind();
-        if kind == "source_file" && !cursor.goto_first_child() {
-            break;
-        }
-        if kind == "section" {
-            if let Some(section) = current_section.take() {
-                result.push(section);
-            }
-
-            if !cursor.goto_first_child() || !cursor.goto_next_sibling() {
-                break;
-            }
-            current_section = Some(Section {
-                name: cursor.node().utf8_text(content).unwrap().to_owned(),
-                statements: vec![],
-            });
-
-            if !cursor.goto_next_sibling() || !cursor.goto_next_sibling() {
-                break;
-            }
-        }
-        if kind == "statement" {
-            if !cursor.goto_first_child() {
-                break;
-            }
-            let key = cursor.node().utf8_text(content).unwrap().to_owned();
-
-            if !cursor.goto_next_sibling() || !cursor.goto_next_sibling() {
-                break;
-            }
-            let value = cursor.node().utf8_text(content).unwrap().to_owned();
-
-            current_section
-                .as_mut()
-                .unwrap()
-                .statements
-                .push(Statement { key, value });
-
-            if !cursor.goto_parent() {
-                break;
-            }
-            if !cursor.goto_next_sibling() && (!cursor.goto_parent() || !cursor.goto_next_sibling())
-            {
-                break;
-            }
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            content: &self.content,
+            cursor: self.tree.walk(),
         }
     }
+}
 
-    result
+fn parse_section_name<'a>(content: &'a str, cursor: &mut TreeCursor<'a>) -> Option<EDSNode<'a>> {
+    if cursor.goto_first_child()
+        && cursor.goto_next_sibling()
+        && cursor.node().kind() == "section_name"
+    {
+        let node = cursor.node();
+        let content = cursor.node().utf8_text(content.as_bytes()).ok()?;
+
+        return Some(EDSNode { content, node });
+    }
+    None
+}
+
+fn parse_statement<'a>(content: &'a str, cursor: &mut TreeCursor<'a>) -> Option<EDSNode<'a>> {
+    if cursor.goto_next_sibling() && cursor.node().kind() == "statement" {
+        let node = cursor.node();
+        let content = cursor.node().utf8_text(content.as_bytes()).ok()?;
+
+        return Some(EDSNode { content, node });
+    }
+    None
+}
+
+impl<'a> Iterator for EDSIterator<'a> {
+    type Item = EDSNode<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let kind = self.cursor.node().kind();
+        let cursor = &mut self.cursor;
+
+        if kind == "source_file" && cursor.goto_first_child() {
+            return self.next();
+        } else if cursor.node().kind() == "section" {
+            return parse_section_name(self.content, cursor);
+        } else if kind == "section_name" {
+            if cursor.goto_next_sibling() {
+                if let Some(node) = parse_statement(self.content, cursor) {
+                    return Some(node);
+                } else if cursor.goto_parent() && cursor.goto_next_sibling() {
+                    return self.next();
+                }
+            }
+        } else if kind == "statement" {
+            if let Some(node) = parse_statement(self.content, cursor) {
+                return Some(node);
+            }
+            if cursor.goto_parent()
+                && cursor.goto_next_sibling()
+                && cursor.node().kind() == "section"
+            {
+                return parse_section_name(self.content, cursor);
+            }
+        }
+        None
+    }
 }
 
 fn main() {
-    let mut parser = eds_parser();
     let content = fs::read_to_string("example.eds").unwrap();
-    let tree = parser.parse(&content, None).unwrap();
+    let eds = EDS::parse(&content).unwrap();
 
-    let node = tree.root_node();
-    println!("{:?}", parse_eds(node, content.as_bytes()));
+    for node in eds.into_iter() {
+        println!("{:?}", node);
+    }
 }
