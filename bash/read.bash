fe25fe9bd46581555d2952298bea55ffbc67c26f
Uy Ha <hchanuy@gmail.com>
2024-04-17 17:31:31 +0000

    more

--- bash/read.bash
+++ bash/read.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 while read -r value; do
-  wc -c <<<"$value"
+  if [[ -z "$value" ]]; then continue; fi
+  printf "%s" "$value" | wc -c
+  printf "'%s'\n" "$value"
 done <<EOF
+  a aa aaa   
+a  asdf
+
   a aa aaa   
 EOF
