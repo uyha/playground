21ed58237390436b5f70367daa3dfba992e141a9
Uy Ha <hchanuy@gmail.com>
2024-10-05 20:18:56 +0000

    more

--- bash/array.bash
+++ bash/array.bash
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-array=(1 2 3)
+array=(1 2 3a)
 array+=(4 5 6)
 
 if [[ 5 == "${array[-1]}" ]]; then
   echo 1
 fi
+
+array=("${array[@]/#/\"}")
+array=("${array[@]/%/\"}")
+
+echo "${array[@]}"
