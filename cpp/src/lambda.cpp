4707233ca4fa839daac45f82ce5946fe28725506
Uy Ha <hchanuy@gmail.com>
2025-04-10 05:26:34 +0000

    more

--- cpp/src/lambda.cpp
+++ cpp/src/lambda.cpp
@@ -1,6 +1,26 @@
+#include <fmt/core.h>
 #include <type_traits>
+
+class A {
+public:
+  struct B {
+    auto call(A const &a) -> void {
+      a.print();
+    }
+  };
+
+private:
+  auto print() const -> void {
+    fmt::print("{}:{}\n", __FILE__, __LINE__);
+    (void)::fflush(::stdout);
+  }
+};
 
 int main() {
   auto lambda = [] {};
   static_assert(std::is_convertible_v<decltype(lambda), auto (*)()->void>);
+
+  auto a = A{};
+  auto b = A::B{};
+  b.call(a);
 }
