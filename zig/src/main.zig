41371f56f7d1fbf81c74e3ce481980416b2d6f41
Uy Ha <hchanuy@gmail.com>
2025-08-18 12:17:11 +0300

    more

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,13 +1,11 @@
-fn Wrap(T: type) type {
-    return struct {
-        fn wtf(self: Wrap(T)) void {
-            std.debug.print("{}\n", .{self});
-        }
-    };
-}
-pub fn main() !void {
-    const wtf: Wrap(u8) = .{};
-    wtf.wtf();
+pub fn main() void {
+    while (true) : ({
+        std.debug.print("{s}:{} ({s})\n", .{ @src().file, @src().line, @src().fn_name });
+        break;
+    }) {
+        defer std.debug.print("{s}:{} ({s})\n", .{ @src().file, @src().line, @src().fn_name });
+        std.debug.print("{s}:{} ({s})\n", .{ @src().file, @src().line, @src().fn_name });
+    }
 }
 
 const std = @import("std");
