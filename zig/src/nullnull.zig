cb4dc732de390ed6991b2e6959b51f231a701474
Uy Ha <hchanuy@gmail.com>
2025-08-27 23:14:08 +0300

    disable nullnull for now

--- zig/src/nullnull.zig
+++ zig/src/nullnull.zig
@@ -1,11 +1,11 @@
 pub fn main() !void {
-    var value: ??u8 = null;
-
-    std.debug.print("{}\n", .{value == null});
-    value = @as(?u8, null);
-    std.debug.print("{}\n", .{value != null});
-    value = 1;
-    std.debug.print("{}\n", .{value == 1});
+    // var value: ??u8 = null;
+    //
+    // std.debug.print("{}\n", .{value == null});
+    // value = @as(?u8, null);
+    // std.debug.print("{}\n", .{value != null});
+    // value = 1;
+    // std.debug.print("{}\n", .{value == 1});
 }
 
 const std = @import("std");
