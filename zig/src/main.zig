313e04153b211a6f03a654d54c75bc028352e256
Uy Ha <hchanuy@gmail.com>
2025-10-29 23:40:16 +0200

    more

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -8,9 +8,31 @@ const A = packed union {
     b: f32,
 };
 
+fn rangeArrayLen(
+    comptime start: usize,
+    comptime end: usize,
+    comptime step: usize,
+) usize {
+    return (end - start) / step + if ((end - start) % step > 0) 1 else 0;
+}
+fn rangeArray(
+    comptime start: usize,
+    comptime end: usize,
+    comptime step: usize,
+) [rangeArrayLen(start, end, step)]usize {
+    var result: [rangeArrayLen(start, end, step)]usize = undefined;
+    var current = start;
+
+    for (0..result.len) |i| {
+        result[i] = current;
+        current += step;
+    }
+
+    return result;
+}
+
 pub fn main() !void {
-    const a: *const [0:0]u8 = "";
-    std.debug.print("{s}\n", .{@typeName(@TypeOf(a))});
+    std.debug.print("{any}\n", .{rangeArray(0, 11, 2)});
 }
 
 const std = @import("std");
