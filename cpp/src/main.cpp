7335273c11e0dca320109b41e1b4257269415600
Uy Ha <hchanuy@gmail.com>
2025-11-02 18:37:05 +0200

    more

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,6 +1,18 @@
-int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
+#include <fmt/core.h>
+#include <mutex>
+
+auto get_unique_inproc_endpoint(std::string_view prefix) -> std::string {
+  static auto count = std::size_t{0};
+  static auto mutex = std::mutex{};
+
+  auto lock = std::scoped_lock{mutex};
+
+  return fmt::format("inproc://{}-{}", prefix, count++);
+}
+
+int main() {
+  fmt::print("{}\n", get_unique_inproc_endpoint("pre").c_str());
+  fmt::print("{}\n", get_unique_inproc_endpoint("asdf"));
 
   return 0;
 }
