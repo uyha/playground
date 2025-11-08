92701662bbb91f0d7bdcea9dccf43ffb034d9bce
Uy Ha <hchanuy@gmail.com>
2025-11-08 13:09:17 +0200

    more

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -4,12 +4,20 @@ const ComptimeField = struct {
 
 fn slice(_: []const i32) void {}
 
+fn throw(err: type, e: ?err) err!void {
+    return e orelse {};
+}
+
 pub fn main() !void {
-    const a: ?i32 = 1;
-    std.debug.print("{}\n", .{&a});
-    if ((&a).*) |*ptr| {
-        std.debug.print("{}\n", .{ptr});
-    }
+    ({
+        errdefer |e| {
+            std.debug.print("{s}:{} ({s}) {}\n", .{ @src().file, @src().line, @src().fn_name, e });
+        }
+
+        return error.WTF;
+    }) catch |e| {
+        std.debug.print("{}\n", .{e});
+    };
 }
 
 const std = @import("std");
