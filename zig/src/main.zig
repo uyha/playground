158f3cdb0847f17f2bc137fb271026517c76a4a0
Uy Ha <hchanuy@gmail.com>
2025-11-20 08:10:44 +0200

    use juicy main

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,11 +1,21 @@
-pub fn main() !void {
-    const clock = std.Io.Clock;
-
-    var threaded: std.Io.Threaded = .init_single_threaded;
-    const io: std.Io = threaded.io();
-
-    const now = try clock.real.now(io);
+fn juicyMain(gpa: Allocator, io: Io) !void {
+    _ = &gpa;
+    const now = try Clock.real.now(io);
     std.debug.print("{}\n", .{now});
 }
 
+pub fn main() !void {
+    var allocator: std.heap.DebugAllocator(.{}) = .init;
+    defer _ = allocator.deinit();
+    const gpa = allocator.allocator();
+
+    var runtime: std.Io.Threaded = .init(gpa);
+    defer runtime.deinit();
+
+    try juicyMain(gpa, runtime.io());
+}
+
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Io = std.Io;
+const Clock = Io.Clock;
