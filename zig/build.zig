a214e79d434dec0b5d839845cf9a119a523252c9
Uy Ha <hchanuy@gmail.com>
2026-02-05 09:57:18 +0200

    more

--- zig/build.zig
+++ zig/build.zig
@@ -1,5 +1,3 @@
-const std = @import("std");
-
 const Options = struct {
     target: std.Build.ResolvedTarget,
     optimize: std.builtin.OptimizeMode,
@@ -38,95 +36,97 @@ pub fn build(b: *std.Build) void {
 
     const options: Options = .{ .target = target, .optimize = optimize };
 
-    const scripts = .{
-        "2d",
-        "align",
-        "arena_usage",
-        "bitcast",
-        "distinct",
-        "enum_map",
-        "errdefer",
-        "fanotify",
-        "for-break",
-        "format",
-        "function",
-        "higheropaque",
-        "ignore",
-        "io",
-        "lambda",
-        "max",
-        "nullnull",
-        "order_of_eval",
-        "packed",
-        "plus",
-        "random",
-        "remove",
-        "sentinel",
-        "signal",
-        "sizeof",
-        "slice",
-        "static",
-        "struct",
-        "subset",
-        "swap",
-        "tagged_union",
-        "tuple",
-        "typemap",
-        "typevar",
-        "union",
-        "use_shared_var",
-        "vector",
-        "writer",
-    };
+    if (builtin.zig_version.minor >= 16) {
+        const scripts = .{
+            "2d",
+            "align",
+            "arena_usage",
+            "bitcast",
+            "distinct",
+            "enum_map",
+            "errdefer",
+            "fanotify",
+            "for-break",
+            "format",
+            "function",
+            "higheropaque",
+            "ignore",
+            "io",
+            "lambda",
+            "max",
+            "nullnull",
+            "order_of_eval",
+            "packed",
+            "plus",
+            "random",
+            "remove",
+            "sentinel",
+            "signal",
+            "sizeof",
+            "slice",
+            "static",
+            "struct",
+            "subset",
+            "swap",
+            "tagged_union",
+            "tuple",
+            "typemap",
+            "typevar",
+            "union",
+            "use_shared_var",
+            "vector",
+            "writer",
+        };
 
-    inline for (scripts) |s| {
-        _ = addPlayground(b, s, options);
-    }
+        inline for (scripts) |s| {
+            _ = addPlayground(b, s, options);
+        }
 
-    const zeromq = addPlayground(b, "zeromq", options);
-    const zimq = b.dependency("zimq", .{
-        .target = target,
-        .optimize = optimize,
-    });
-    const mzg = b.dependency("mzg", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    zeromq.root_module.addImport("zimq", zimq.module("zimq"));
-    zeromq.root_module.addImport("mzg", mzg.module("mzg"));
-
-    const publish = addPlayground(b, "publish", options);
-    publish.root_module.addImport("zimq", zimq.module("zimq"));
-
-    const subscribe = addPlayground(b, "subscribe", options);
-    subscribe.root_module.addImport("zimq", zimq.module("zimq"));
-
-    const message_pack = addPlayground(b, "message_pack", options);
-    message_pack.root_module.addImport("mzg", mzg.module("mzg"));
-
-    const test_mod = b.addTest(.{
-        .name = "test",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/test.zig"),
+        const zeromq = addPlayground(b, "zeromq", options);
+        const zimq = b.dependency("zimq", .{
             .target = target,
             .optimize = optimize,
-        }),
-    });
-    test_mod.root_module.addImport("zimq", zimq.module("zimq"));
-    const run_tests = b.addRunArtifact(test_mod);
-    const test_step = b.step("test", "test");
-    test_step.dependOn(&run_tests.step);
+        });
+        const mzg = b.dependency("mzg", .{
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const copyRes = b.addSystemCommand(&.{"cp"});
-    copyRes.addFileArg(b.path("res/some.txt"));
-    const out = copyRes.addOutputFileArg("out/some.txt");
+        zeromq.root_module.addImport("zimq", zimq.module("zimq"));
+        zeromq.root_module.addImport("mzg", mzg.module("mzg"));
 
-    const resource = addPlayground(b, "resource", options);
-    resource.root_module.addAnonymousImport("some", .{ .root_source_file = out });
+        const publish = addPlayground(b, "publish", options);
+        publish.root_module.addImport("zimq", zimq.module("zimq"));
 
-    const clock = addPlayground(b, "clock", options);
-    clock.root_module.link_libc = true;
+        const subscribe = addPlayground(b, "subscribe", options);
+        subscribe.root_module.addImport("zimq", zimq.module("zimq"));
+
+        const message_pack = addPlayground(b, "message_pack", options);
+        message_pack.root_module.addImport("mzg", mzg.module("mzg"));
+
+        const test_mod = b.addTest(.{
+            .name = "test",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/test.zig"),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        test_mod.root_module.addImport("zimq", zimq.module("zimq"));
+        const run_tests = b.addRunArtifact(test_mod);
+        const test_step = b.step("test", "test");
+        test_step.dependOn(&run_tests.step);
+
+        const copyRes = b.addSystemCommand(&.{"cp"});
+        copyRes.addFileArg(b.path("res/some.txt"));
+        const out = copyRes.addOutputFileArg("out/some.txt");
+
+        const resource = addPlayground(b, "resource", options);
+        resource.root_module.addAnonymousImport("some", .{ .root_source_file = out });
+
+        const clock = addPlayground(b, "clock", options);
+        clock.root_module.link_libc = true;
+    }
 
     const main = addPlayground(b, "main", options);
     main.root_module.addAnonymousImport(
@@ -134,3 +134,6 @@ pub fn build(b: *std.Build) void {
         .{ .root_source_file = b.path("src/res.zon") },
     );
 }
+
+const std = @import("std");
+const builtin = @import("builtin");
--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,13 +1,6 @@
-const Tup = struct { u8, u16, bool };
-fn T(comptime index: usize) @TypeOf(@as(Tup, undefined)[index]) {
-    return if (index != 2) 0 else false;
+pub fn main(init: Init) !void {
+    try std.Io.File.stdout().writeStreamingAll(init.io, "Hello, World!\n");
 }
-pub fn main() !void {
-    std.debug.print("{}\n", .{T(2)});
-    init();
-}
-
-pub extern fn init() void;
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
