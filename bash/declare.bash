13039fdfdf2ec835e254b8882ba7a37cd48ae0f3
Uy Ha <hchanuy@gmail.com>
2026-08-31 10:46:47 +0300

    more

--- bash/declare.bash
+++ bash/declare.bash
@@ -2,6 +2,21 @@
 
 set -euo pipefail
 
-declare val
+contains() {
+  declare -n haystack
+  haystack=$1
+  needle=$2
 
-echo "${val:?}"
+  for item in "${haystack[@]}"; do
+    if [[ "${needle}" == "${item}" ]]; then return 0; fi
+  done
+
+  false
+}
+
+array=(1 2 3)
+if contains array 3; then
+  echo yes
+else
+  echo no
+fi
